d58dad6b954a8f721e4f52d6d665e00a9c4fad30
Mostafa Yassin <mostafakaram485@gmail.com>
2026-09-12 19:32:12 +0300

    event halls and daily rent bookings and wallet money holds and dark mode

--- lib/features/account/presentation/screens/account_screen.dart
+++ lib/features/account/presentation/screens/account_screen.dart
@@ -3,7 +3,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
+//commit
 import '../../../../core/constants/app_constants.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_text_styles.dart';
